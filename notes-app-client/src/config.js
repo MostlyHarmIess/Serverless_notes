39528aa87b2fe7670e771442c865399ece989eb6
Edward Shannon <edward@shannon.net>
2020-06-11 10:39:15 +0100

    notes adding, editing and deleting added

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -1,4 +1,5 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "ew-west-2",
       BUCKET: "eds-notes-app-uploads"
@@ -10,7 +11,7 @@ export default {
     cognito: {
       REGION: "eu-west-2",
       USER_POOL_ID: "eu-west-2_gh576OWu9",
-      APP_CLIENT_ID: "Y150tvni0u9oal9j9nr5jm2o92k",
+      APP_CLIENT_ID: "150tvni0u9oal9j9nr5jm2o92k",
       IDENTITY_POOL_ID: "eu-west-2:145e2e9b-4259-44e7-b9be-5f6a96a2d929"
     }
   };
